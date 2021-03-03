d683371bfc719d1360f42d418b74b6f99c422beb
Lukas Puckerin <lukaspuckerin@gmail.com>
2021-03-03 16:08:59 -0500

    rg

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = [ 'leadcopyme23.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'whitenoise.runserver_nostatic',
     'leads',
     'rest_framework',
     'frontend',
