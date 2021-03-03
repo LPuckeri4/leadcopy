5fd01e70976ce1c38922e8050ec6ad259a333078
Lukas Puckerin <lukaspuckerin@gmail.com>
2021-03-03 15:57:01 -0500

    rgrrg

--- leadmanager/wsgi.py
+++ leadmanager/wsgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leadmanager.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+application = DjangoWhiteNoise(application)
 
